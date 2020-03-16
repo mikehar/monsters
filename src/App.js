8ab9682e49e4343cac39ebe68d167a4a5e5d81b3
mikehar <mikehar@gmail.com>
2020-03-16 08:14:47 -0700

    cores support

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users', {mode: 'cors'})
     .then(response => response.json())
     .then(users => this.setState({monsters: users}));
   }
