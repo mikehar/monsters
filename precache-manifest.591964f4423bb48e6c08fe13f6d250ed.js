4efbfc28489295a28758890d1c9aab169afaddae
mikehar <mikehar@gmail.com>
2020-03-16 08:05:13 -0700

    Updates

---
+++ precache-manifest.591964f4423bb48e6c08fe13f6d250ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3db0c6faa82e6d30b59d5f18d9f8d42",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b36a190cd8f02c90529a",
+    "url": "/monsters/static/css/main.e79ac2c6.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36a190cd8f02c90529a",
+    "url": "/monsters/static/js/main.994a220c.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
